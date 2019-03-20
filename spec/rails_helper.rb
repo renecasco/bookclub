138c66ae758d0a5228cd8dce5b5cabaa278b4ad3
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-03-19 21:15:18 -0600

    Set up gitignore and added simplecov
    Co-authored-by: Rene Casco <34440390+renecasco@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -59,6 +60,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+require 'simplecov'
+SimpleCov.start
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
