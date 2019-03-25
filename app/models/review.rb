31984a6e69156fb9e411a941243864b9c8adae5d
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-03-24 20:37:55 -0600

    Adds test and functionality for users with most reviews
    Co-authored-by: Rene Casco <34440390+renecasco@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,11 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true, numericality: {
    greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  def self.users_with_most_reviews
+    Review.group(:user)
+          .select("user", "COUNT(*) as count")
+          .order("count desc")
+          .limit(3)
+  end
 end
