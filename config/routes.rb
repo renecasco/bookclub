5ced14f1349827e445453669e825949355e02ff4
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-03-25 14:26:44 -0600

    adds tests and functionality for User Story 16

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:index, :new, :create]
   end
+  resources :users, only: [:show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-author_1 = Author.create(name: "Martin Short")
+# author_1 = Author.create(name: "Martin Short")
+# author_2 = Author.create(name: "J.R.R. Tolkein")
+# author_3 = Author.create(name: "Steve Martin")
+#
+# book_1 = Book.create(title: "How to Funny", pages: 3, publication_year: 2000, cover_art: "https://images-na.ssl-images-amazon.com/images/I/41PG42Z25GL._SX323_BO1,204,203,200_.jpg", authors: [author_1, author_3])
+# review_1 = book_1.reviews.create(user: "smitty", title: "Hahaha!", rating: 5, description: "it was actually funny!")
+# review_2 = book_1.reviews.create(user: "renny", title: "boo hoo hoo", rating: 1, description: "it wasn't funny!")
+#
+# book_2 = Book.create(title: "The Hobbit", pages: 478, publication_year: 1932, cover_art: "https://images-na.ssl-images-amazon.com/images/I/51wScUt0gZL._SX329_BO1,204,203,200_.jpg", authors: [author_2])
+# review_3 = book_2.reviews.create(user: "smitty", title: "Furry Feet!", rating: 5, description: "I can see why this is a classic!")
+# review_4 = book_2.reviews.create(user: "renny", title: "novel was too short?", rating: 2, description: "That's a lot of pages for short people")
+
+author_1 = Author.create(name: "Jane Austen")
 author_2 = Author.create(name: "J.R.R. Tolkein")
 author_3 = Author.create(name: "Steve Martin")
 
-book_1 = Book.create(title: "How to Funny", pages: 3, publication_year: 2000, cover_art: "https://images-na.ssl-images-amazon.com/images/I/41PG42Z25GL._SX323_BO1,204,203,200_.jpg", authors: [author_1, author_3])
-review_1 = book_1.reviews.create(user: "smitty", title: "Hahaha!", rating: 5, description: "it was actually funny!")
-review_2 = book_1.reviews.create(user: "renny", title: "boo hoo hoo", rating: 1, description: "it wasn't funny!")
-
+book_1 = Book.create(title: "Pride and Prejudice", pages: 278, publication_year: 1797, cover_art: "https://images-na.ssl-images-amazon.com/images/I/51wScUt0gZL._SX329_BO1,204,203,200_.jpg", authors: [author_1])
 book_2 = Book.create(title: "The Hobbit", pages: 478, publication_year: 1932, cover_art: "https://images-na.ssl-images-amazon.com/images/I/51wScUt0gZL._SX329_BO1,204,203,200_.jpg", authors: [author_2])
-review_3 = book_2.reviews.create(user: "smitty", title: "Furry Feet!", rating: 5, description: "I can see why this is a classic!")
-review_4 = book_2.reviews.create(user: "renny", title: "novel was too short?", rating: 2, description: "That's a lot of pages for short people")
+book_3 = Book.create(title: "Shop Girl", pages: 150, publication_year: 2010, cover_art: "https://images-na.ssl-images-amazon.com/images/I/41PG42Z25GL._SX323_BO1,204,203,200_.jpg", authors: [author_3, author_1])
+
+review_1 = book_1.reviews.create(user: "smitty", title: "Mr. Darcy!", rating: 5, description: "Did I mention, Mr. Darcy?!")
+review_2 = book_1.reviews.create(user: "renny", title: "boo hoo hoo", rating: 1, description: "it wasn't funny!")
+review_3 = book_3.reviews.create(user: "smitty", title: "Furry Feet!", rating: 5, description: "I can see why this is a classic!")
+review_4 = book_3.reviews.create(user: "renny_1", title: "novel was too short?", rating: 2, description: "That's a lot of pages for short people")
+review_5 = book_1.reviews.create(user: "renny_2", title: "Gold Diggers!", rating: 2, description: "Get a job! Even though I know you are not allowed to work.")
